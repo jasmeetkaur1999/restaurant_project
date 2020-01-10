54532de8de2a06a0f0818b184dc96cd8acfc37b3
jasmeet kaur <jasmeet.bjf@gmail.com>
2020-01-10 10:29:46 -0600

    changes in dbhelper file

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,8 +150,9 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
+    return (`https://github.com/jasmeetkaur1999/restaurant_project.git/master/img/${restaurant.photograph}`)
    // return (`/img/${restaurant.photograph}`);
-   return (`https://github.com/jasmeetkaur1999/restaurant_project.git/master/img/${restaurant.photograph}`)
+  
   }
 
   /**

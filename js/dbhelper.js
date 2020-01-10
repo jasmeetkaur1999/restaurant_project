2337eb0227e195dffd405da78adb312aad2dae8f
jasmeet kaur <jasmeet.bjf@gmail.com>
2020-01-09 22:26:49 -0600

    add images url

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,7 @@ class DBHelper {
    */
   static imageUrlForRestaurant(restaurant) {
    // return (`/img/${restaurant.photograph}`);
-   return (`https://jasmeetkaur1999.github.io/restaurant_project/master/img/${restaurant.photograph}`)
+   return (`https://github.com/jasmeetkaur1999/restaurant_project.git/master/img/${restaurant.photograph}`)
   }
 
   /**

819ed91959a0960118bc1748c0fbf4b087f8643d
jasmeet kaur <jasmeet.bjf@gmail.com>
2020-01-10 11:32:09 -0600

    sfdg

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -10,7 +10,7 @@ class DBHelper {
   static get DATABASE_URL() {
     const port = 5500 // Change this to your server port
    //return `http://localhost:${port}/data/restaurants.json`;
-    return  `https://jasmeetkaur1999.github.io/restaurant_project/${port}/data/restaurants.json`
+    return  `https://jasmeetkaur1999.github.io/restaurant_project/data/restaurants.json`
   }
 
   /**
